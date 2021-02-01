908898305038cafcd0b6ab5d14ef840aa7efe543
creativpixie <kosherna12@gmail.com>
2021-02-01 17:25:46 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Длина массива";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(262, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(401, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(109, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 137);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 277);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(195, 137);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(151, 199);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 420);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 12;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(368, 137);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(142, 199);
+            this.listBox3.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(228, 448);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 18;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(553, 137);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(142, 199);
+            this.listBox4.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(398, 389);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(398, 420);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(398, 448);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(584, 448);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(584, 420);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 21;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(584, 389);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Начальный диапазон";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(398, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Конечный диапазон";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Неотсортированный лист";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(237, 339);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(66, 13);
+            this.label14.TabIndex = 26;
+            this.label14.Text = "Пузырьком";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(414, 339);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(40, 13);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "Шелла";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(601, 339);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(55, 13);
+            this.label16.TabIndex = 28;
+            this.label16.Text = "Вставкой";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 482);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void fillNumbers()
+        {
+
+            int massiveCount = Convert.ToInt32(textBox1.Text);
+            int minNumber = Convert.ToInt32(textBox2.Text);
+            int maxNumber = Convert.ToInt32(textBox3.Text);
+            int[] arrayNums = new int[massiveCount];
+            Random random = new Random();
+            for (int i = 0; i < massiveCount; i++)
+            {
+                int nums = random.Next(minNumber - 1, maxNumber + 1);
+                arrayNums[i] = Convert.ToInt32(listBox1.Items.Add(nums));
+            }
+            foreach (var i in listBox1.Items)
+            {
+                listBox2.Items.Add(i);
+                listBox3.Items.Add(i);
+                listBox4.Items.Add(i);
+            }
+        }
+        private void sortBubble()
+        {
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            int temp;
+            int pass = 0;
+            int swap = 0;
+                
+            for (int j = 0; j <= listBox2.Items.Count-1; j++)
+                {
+                    for (int i = 0; i <= listBox2.Items.Count -2; i++)
+                    {
+                         pass++;
+                        if (Convert.ToInt32(listBox2.Items[i]) > Convert.ToInt32(listBox2.Items[i + 1]))
+                        {
+                            swap++;
+                            temp = Convert.ToInt32(listBox2.Items[i + 1]);
+                            listBox2.Invoke(new Action(() => listBox2.Items[i + 1] = Convert.ToInt32(listBox2.Items[i])));
+                            listBox2.Invoke(new Action(() => listBox2.Items[i] = temp));
+                        }
+                    }
+                }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+             ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            label3.Invoke(new Action(() => label2.Text = elapsedTime));
+            label2.Invoke(new Action(() => label3.Text = pass.ToString() + "  " + swap.ToString()));
+            label7.Invoke(new Action(() => label7.Text = "Сортировка закончена"));
+        }
+        private void shellSort()
+        {
+            System.Diagnostics.Stopwatch stopWatch2 = new System.Diagnostics.Stopwatch();
+            stopWatch2.Start();
+             
+            int i, j, inc, temp;
+            inc = 3;
+            int pass2 = 0;
+            int swap2 = 0;
+            while (inc > 0)
+            {
+                for (i = 0; i < listBox3.Items.Count; i++)
+                {
+                    pass2++;
+                    j = i;
+                    temp = Convert.ToInt32(listBox3.Items[i]);
+                    while ((j >= inc) && ((Convert.ToInt32(listBox3.Items[j - inc]) > temp)))
+                    {
+                        listBox3.Invoke(new Action(() => listBox3.Items[j] = Convert.ToInt32(listBox3.Items[j - inc])));
+                        j = j - inc;
+                        swap2++;
+                    }
+                    listBox3.Invoke(new Action(() => listBox3.Items[j] = temp));
+                }
+                if (inc / 2 != 0)
+                    inc = inc / 2;
+                else if (inc == 1)
+                    inc = 0;
+                else
+                    inc = 1;
+            }
+            stopWatch2.Stop();
+            TimeSpan ts2 = stopWatch2.Elapsed;
+            string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+             ts2.Hours, ts2.Minutes, ts2.Seconds,
+            ts2.Milliseconds / 10);
+                label8.Invoke(new Action(() => label8.Text = elapsedTime2));
+                label9.Invoke(new Action(() => label9.Text = pass2.ToString() + "  " + swap2.ToString()));
+                label10.Invoke(new Action(() => label10.Text = "Сортировка закончена"));
+        }
+
+        private void InsertionSort()
+        {
+            System.Diagnostics.Stopwatch stopWatch3 = new System.Diagnostics.Stopwatch();
+            stopWatch3.Start();
+            int pass3 = 0;
+            int swap3 = 0;
+            for (int i = 0; i < listBox4.Items.Count - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    pass3++;
+                    if (Convert.ToInt32(listBox4.Items[j - 1]) > Convert.ToInt32(listBox4.Items[j]))
+                    {
+                        int temp = Convert.ToInt32(listBox4.Items[j - 1]);
+                        listBox4.Invoke(new Action(() => listBox4.Items[j - 1] = Convert.ToInt32(listBox4.Items[j])));
+                        listBox4.Invoke(new Action(() => listBox4.Items[j] = temp));
+                        swap3++;
+                    }
+                }
+            }
+
+            stopWatch3.Stop();
+            TimeSpan ts3 = stopWatch3.Elapsed;
+            string elapsedTime3 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+             ts3.Hours, ts3.Minutes, ts3.Seconds,
+            ts3.Milliseconds / 10);
+            label13.Invoke(new Action(() => label13.Text = elapsedTime3));
+            label12.Invoke(new Action(() => label12.Text = pass3.ToString() + "  " + swap3.ToString()));
+            label11.Invoke(new Action(() => label11.Text = "Сортировка закончена"));
+        }
+
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            fillNumbers();
+           
+            Thread t = new Thread(sortBubble);
+            t.Start();
+            Thread t2 = new Thread(shellSort);
+            t2.Start();
+            Thread t3 = new Thread(InsertionSort);
+            t3.Start();
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
